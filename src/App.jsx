9622784e17b9faebe148de764f62043be0e98b83
Lance Cruzado <raphaellcruzado@gmail.com>
2023-01-09 23:53:03 -0800

    starting bootstrap stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-// import logo from './logo.svg';
 import './css/styles.css'
+
 function App() {
   return (
     <div className='custom-container d-flex justify-content-center align-items-center vh-100'>
-      <h1>hello you sexy motherfucker</h1>
+      <div className='row align-items-start'>
+        <div className='col-9 calculator-header-color'>hello</div>
+        <div className='col'>
+          1 of 3
+        </div>
+        <div className='col'>
+          2 of 3
+        </div>
+        <div className='col'>
+          3 of 3
+        </div>
+      </div>
+
+
     </div>
   );
 }
 
 export default App;
+
+// {/* <div className='d-flex w-100 justify-content-center'>
+//   <div className='calculator-header-color'>
+//     <span></span>
+//   </div>
+// </div> */}
